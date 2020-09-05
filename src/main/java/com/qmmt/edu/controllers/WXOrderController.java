9d5d3dca4af2495a5a4a5d01ed715746437e84df
hubo <1947390185@qq.com>
2020-09-05 18:24:06 +0800

    初始化

--- src/main/java/com/qmmt/edu/controllers/WXOrderController.java
+++ src/main/java/com/qmmt/edu/controllers/WXOrderController.java
@@ -3,7 +3,6 @@ package com.qmmt.edu.controllers;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
